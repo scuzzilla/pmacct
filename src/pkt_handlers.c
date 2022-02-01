06c4c6b9deef9dc0546bcc34f085c97317641d48
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-02-01 18:23:10 +0100

    prelim test with mpls data struct (11)...

--- src/pkt_handlers.c
+++ src/pkt_handlers.c
@@ -3580,7 +3580,7 @@ void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *
     strcat(pmpls->mpls_label_stack, ",");
 
     int idx_0;
-    for(idx_0 = 1; idx_0 < 4; idx_0++) {
+    for(idx_0 = 1; idx_0 < 3; idx_0++) {
       sprintf(label_buf, "%lu", labels_cicle[idx_0]);
       pmpls->mpls_label_stack = (char *) realloc(pmpls->mpls_label_stack, sizeof(char) * (strlen(label_buf) + 3));
       strcat(pmpls->mpls_label_stack, labels_idx[idx_0]);
