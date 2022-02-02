ddfb4e2e0132fc85b38cef802e8fa467981b777a
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-02-02 15:41:03 +0100

    prelim test with mpls data struct (17)...

--- src/pkt_handlers.c
+++ src/pkt_handlers.c
@@ -3579,6 +3579,7 @@ void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *
         labels_cicle[3] = pmpls->mpls_label_stack_section4;
       }
     }
+    /*
     if (tpl->tpl[NF9_MPLS_LABEL_5].len == 3) {
       pmpls->mpls_label_stack_section5 = decode_mpls_label(pptrs->f_data+tpl->tpl[NF9_MPLS_LABEL_5].off);
       printf("mpls_label_stack_section5: %zu\n", pmpls->mpls_label_stack_section5);
@@ -3593,6 +3594,7 @@ void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *
         labels_cicle[5] = pmpls->mpls_label_stack_section6;
       }
     }
+    */
 
     memset(&label_buf, 0, sizeof(label_buf));
     snprintf(label_buf, MAX_MPLS_LABEL_LEN, "%zu", labels_cicle[0]);
@@ -3615,7 +3617,7 @@ void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *
     }
     
     printf("MPLS_LABEL_STACK: %s\n", pmpls->mpls_label_stack);
-    //free(pmpls->mpls_label_stack);
+    free(pmpls->mpls_label_stack);
 
     break;
   default:
