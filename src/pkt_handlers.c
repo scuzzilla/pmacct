afd32b34af11e8d5a4b4d719051efbd0fee66225
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-02-02 19:56:50 +0100

    prelim test with mpls data struct (20)...

--- src/pkt_handlers.c
+++ src/pkt_handlers.c
@@ -3603,7 +3603,7 @@ void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *
     }
     
     printf("MPLS_LABEL_STACK: %s\n", pmpls->mpls_label_stack);
-    free(pmpls->mpls_label_stack);
+    //free(pmpls->mpls_label_stack);
 
     break;
   default:
