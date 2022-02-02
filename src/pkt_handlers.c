c80b91856375369be5ffe5f75aabcaf1779bf7d9
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-02-02 17:06:46 +0100

    prelim test with mpls data struct (17)...

--- src/pkt_handlers.c
+++ src/pkt_handlers.c
@@ -3591,7 +3591,7 @@ void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *
     memset(&label_buf, 0, sizeof(label_buf));
     snprintf(label_buf, MAX_MPLS_LABEL_LEN, "%zu", labels_cicle[0]);
     pmpls->mpls_label_stack = (char *) malloc(sizeof(char) * (strlen(label_buf) + 3));
-    strcpy(pmpls->mpls_label_stack, labels_idx[0]);
+    strcpy(pmpls->mpls_label_stack, "0");
     strcat(pmpls->mpls_label_stack, "-");
     strcat(pmpls->mpls_label_stack, label_buf);
     strcat(pmpls->mpls_label_stack, ",");
@@ -3600,8 +3600,8 @@ void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *
     for(idx_0 = 1; idx_0 < MAX_MPLS_LABELS; idx_0++) {
       memset(&label_buf, 0, sizeof(label_buf));
       snprintf(label_buf, MAX_MPLS_LABEL_LEN, "%zu", labels_cicle[idx_0]);
-      pmpls->mpls_label_stack = (char *) realloc(pmpls->mpls_label_stack, sizeof(char) * (strlen(label_buf) + 4));
-      strcat(pmpls->mpls_label_stack, labels_idx[1]);
+      pmpls->mpls_label_stack = (char *) realloc(pmpls->mpls_label_stack, sizeof(char) * (strlen(label_buf) + 3));
+      strcat(pmpls->mpls_label_stack, "1");
       strcat(pmpls->mpls_label_stack, "-");
       strcat(pmpls->mpls_label_stack, label_buf);
       strcat(pmpls->mpls_label_stack, ",");
