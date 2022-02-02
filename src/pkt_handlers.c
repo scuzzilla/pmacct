4b9d0e85e777d005ae437988adfd76c46345d0a6
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-02-02 11:59:33 +0100

    prelim test with mpls data struct (14)...

--- src/pkt_handlers.c
+++ src/pkt_handlers.c
@@ -3534,7 +3534,8 @@ void NF_nat_event_handler(struct channels_list_entry *chptr, struct packet_ptrs 
   }
 }
 
-void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *pptrs, char **data)
+//void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *pptrs, char **data)
+void NF_mpls_label_stack()
 {
   struct struct_header_v5 *hdr = (struct struct_header_v5 *) pptrs->f_header;
   struct template_cache_entry *tpl = (struct template_cache_entry *) pptrs->f_tpl;
@@ -3598,7 +3599,7 @@ void NF_mpls_label_stack(struct channels_list_entry *chptr, struct packet_ptrs *
     }
 
     printf("MPLS_LABEL_STACK: %s\n", pmpls->mpls_label_stack);
-    //free(pmpls->mpls_label_stack);
+    free(pmpls->mpls_label_stack);
 
     break;
   default:
