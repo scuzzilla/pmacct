82fea0ea38fcbdca29e7df3b25d8f1b6193300c6
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-02-04 15:44:36 +0100

    prelim test with mpls data struct (27)...

--- src/plugin_cmn_avro.c
+++ src/plugin_cmn_avro.c
@@ -1654,8 +1654,8 @@ int compose_nfacctd_mpls_label_stack_data(u_int32_t *labels_cycle, char *label_b
     if (avro_value_get_by_name(&v_type_record, "mpls_label_stack", &v_type_array, NULL) == 0) {
       if (strcmp(label_buff, "0") != 0) {
         snprintf(idx_buff, 2, "%zu", idx_0);
-        strcat("-", label_buff);
-        strcat(idx_buff, label_buff);
+        strcat(label_buff, "-");
+        strcat(label_buff, idx_buff);
         if (avro_value_append(&v_type_array, &v_type_string, NULL) == 0) {
           avro_value_set_string(&v_type_string, label_buff);
         }
