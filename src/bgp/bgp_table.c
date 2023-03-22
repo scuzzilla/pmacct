3a631dde59ca887e65841ce3c1df994e9724da2b
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2023-03-22 15:49:56 +0100

    adding rd to both BGP & BMP hashing functions (24)...

--- src/bgp/bgp_table.c
+++ src/bgp/bgp_table.c
@@ -278,7 +278,8 @@ bgp_node_match (const struct bgp_table *table, struct prefix *p, struct bgp_peer
   /* Walk down tree.  If there is matched route then store it to matched. */
   while (node && node->p.prefixlen <= p->prefixlen && prefix_match(&node->p, p)) {
     for (local_modulo = modulo, modulo_idx = 0; modulo_idx < modulo_max; local_modulo++, modulo_idx++) {
-      for (info = node->info[local_modulo]; info; info = info->next) {
+      //for (info = node->info[local_modulo]; info; info = info->next) {
+      for (info = node->info[999]; info; info = info->next) {
         if (!cmp_func(info, nmct2)) {
           printf("if (!cmp_func(info, nmct2))\n");
           matched_node = node;
