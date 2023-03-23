fed247bee5ff7e16d379ff574b44b6a07c739e9d
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2023-03-23 09:21:09 +0100

    adding rd to both BGP & BMP hashing functions (24)...

--- src/bgp/bgp_lookup.c
+++ src/bgp/bgp_lookup.c
@@ -634,9 +634,7 @@ int bgp_lookup_node_match_cmp_bgp(struct bgp_info *info, struct node_match_cmp_t
 {
   if (info->peer != nmct2->peer) {
     return 1;
-  }
-
-  if (info->peer == nmct2->peer) {
+  } else {
     if (nmct2->safi == SAFI_MPLS_VPN) {
       if (info->attr_extra && !memcmp(&info->attr_extra->rd, nmct2->rd, sizeof(rd_t))) {
         printf("MATCH RD1 == RD2\n\n\n");
