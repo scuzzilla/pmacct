3cb8d3380ebd546ab558e720d6d54f46be465605
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2023-03-23 07:55:33 +0100

    adding rd to both BGP & BMP hashing functions (24)...

--- src/bgp/bgp_table.c
+++ src/bgp/bgp_table.c
@@ -278,32 +278,33 @@ bgp_node_match (const struct bgp_table *table, struct prefix *p, struct bgp_peer
   if (bnv) bnv->entries = 0;
 
   /* Walk down tree.  If there is matched route then store it to matched. */
+  int ll_walk = 0;
+  int tree_walk = 0;
   while (node && node->p.prefixlen <= p->prefixlen) {
-    //for (local_modulo = modulo, modulo_idx = 0; modulo_idx < modulo_max; local_modulo++, modulo_idx++) {
-      //for (info = node->info[local_modulo]; info; info = info->next) {
-      if (prefix_match(&node->p, p)) {
-        for (info = node->info[modulo]; info; info = info->next) {
-          if (!cmp_func(info, nmct2)) {
-            //printf("if (!cmp_func(info, nmct2))\n");
-            matched_node = node;
-            matched_info = info;
+    tree_walk++;
+    if (prefix_match(&node->p, p)) {
+      for (info = node->info[modulo]; info; info = info->next) {
+        ll_walk++;
+        if (!cmp_func(info, nmct2)) {
+          printf("MATCH - tree: %d, ll: %d\n", tree_walk, ll_walk);
+          matched_node = node;
+          matched_info = info;
 
-            if (bnv) {
-              bnv->v[bnv->entries].p = &node->p;
-              bnv->v[bnv->entries].info = info;
-              bnv->entries++;
-            }
-
-            (*result_node) = matched_node;
-            (*result_info) = matched_info;
-            bgp_lock_node (peer, matched_node);
-
-            //if (node->p.prefixlen == p->prefixlen) break;
-            break;
+          if (bnv) {
+            bnv->v[bnv->entries].p = &node->p;
+            bnv->v[bnv->entries].info = info;
+            bnv->entries++;
           }
+
+          (*result_node) = matched_node;
+          (*result_info) = matched_info;
+          bgp_lock_node (peer, matched_node);
+
+          break;
         }
+        printf("NO-MATCH - tree: %d, ll: %d\n", tree_walk, ll_walk);
       }
-    //}
+    }
     node = node->link[check_bit(&p->u.prefix, node->p.prefixlen)];
   }
 }
