6e94eb1d78d77bfb8f8782748f480f9777377868
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2023-03-23 07:27:20 +0100

    adding rd to both BGP & BMP hashing functions (24)...

--- src/bgp/bgp_lookup.c
+++ src/bgp/bgp_lookup.c
@@ -632,7 +632,7 @@ struct bgp_peer *bgp_lookup_find_bgp_peer(struct sockaddr *sa, struct xflow_stat
 
 int bgp_lookup_node_match_cmp_bgp(struct bgp_info *info, struct node_match_cmp_term2 *nmct2)
 {
-  if (info->peer != criteria->peer) {
+  if (info->peer != nmct2->peer) {
     return 1;
   }
 
