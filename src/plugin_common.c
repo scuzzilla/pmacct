5aaf752991efe08e06135e4b160c23d60f5eaaae
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2023-09-29 08:32:22 +0200

    pr_729_fix (1) ...

--- src/plugin_common.c
+++ src/plugin_common.c
@@ -1138,7 +1138,7 @@ cdada_list_t *generic_delim_str_to_linked_list(const char *delimited_string, con
 
   /* Safer to work on a copy of the original string */
   char delimited_string_cpy[max_delimited_string_len];
-  strncpy(delimited_string_cpy, delimited_string, max_delimited_string_len);
+  strncpy(delimited_string_cpy, delimited_string, MAX_GENERIC_DELIM_STR_TOKEN_LEN - 1);
   delimited_string_cpy[max_delimited_string_len - 1] = '\0';
 
   char *saveptr = NULL;
